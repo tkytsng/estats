f48840d7a3037b53b478feb40627fa8358cf0026
unknown <lisdrc@snet.isid.co.jp>
2019-01-24 10:46:05 +0900

    init

---
+++ e-stats/src/components/Chart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Line, Bar, mixins } from "vue-chartjs";
+export default {
+  name: "Chart",
+  // mixins: [mixins.reactiveProp],
+  extends: Line,
+  props: { chartData: Object },
+  data() {
+    return {
+      // options: {
+      //   title: {
+      //     display: true
+      //   },
+      //   responsive: true, 
+      //   maintainAspectRatio: true,
+      // }
+    };
+  },
+  mounted: function() {
+    this.renderChart(this.chartData.data);
+    // console.log(this.chartData.data)
+  },
+  watch: {
+    chartData: function() {
+      this.renderChart(this.chartData.data);
+    }
+  }
+};
+</script>
